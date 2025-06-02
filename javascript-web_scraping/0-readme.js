c522651a60cdbd38522df0486c0d40b1b620fae0
innocent-gift <i.nkurunziz@alustudent.com>
2025-06-02 13:00:45 +0200

    this is t

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,10 +2,15 @@
 const fs = require('fs');
 const file = process.argv.slice(2)[0];
 
+if (!file) {
+  console.error('Usage: ./script.js <filename>');
+  process.exit(1);
+}
+
 fs.readFile(file, 'utf8', (err, contents) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(contents);
+    console.error(err);
+    process.exit(1);
   }
+  console.log(contents);
 });
